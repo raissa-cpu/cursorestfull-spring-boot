54d06815d8875ae7faacc40b9a3530f1e8a06443
Raissa <raissaliramiranda@hotmail.com>
2020-09-13 13:00:58 -0300

    #1234 resources /cliente / {id}

---
+++ src/main/java/com/example/cursorestfulspringboot/controler/Cliente.java
@@ -0,0 +1,11 @@
+package com.example.cursorestfulspringboot.controler;
+
+public class Cliente {
+
+    public int id;
+    public String nome;
+    public String endereco;
+    public double saldo;
+    
+}
+
--- src/main/java/com/example/cursorestfulspringboot/controler/ClienteControle.java
+++ src/main/java/com/example/cursorestfulspringboot/controler/ClienteControle.java
@@ -1,20 +1,58 @@
 package com.example.cursorestfulspringboot.controler;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteControle {
+
+    public List<Cliente> clientes;
+
+    @PostConstruct
+    public void init(){
+        Cliente c1 = new Cliente();
+        c1.id = 1;
+        c1.nome = "Jose";
+        c1.endereco = "Rua X, 99";
+        c1.saldo = 202.0;
+
+        Cliente c2 = new Cliente();
+        c2.id = 2;
+        c2.nome = "Maria";
+        c2.endereco = "Rua Y, 59";
+        c2.saldo = 404.0;
+
+        Cliente c3 = new Cliente();
+        c3.id = 3;
+        c3.nome = "Fernanda";
+        c3.endereco = "Rua W, 33";
+        c3.saldo = 332.0;
+
+        clientes = Arrays.asList(c1,c2,c3);
+    }
     
     @GetMapping("/clientes")
-        public String getClientes(){
-            return "Vai retornar todos os clientes";
+        public List<Cliente> getClientes(){
+            return clientes;
         }
     
+    @GetMapping("/clientes/{id}")
+        public Cliente getClienteByCodigo(@PathVariable int id){
+            Cliente cliente = null;
 
-    @GetMapping("/clientes/{codigo}")
-        public String getClienteByCodigo(@PathVariable int codigo){
-            return "Vai retornar o cliente de codigo: " + codigo;
+            for(Cliente aux : clientes){
+                if(aux.id == id){
+                    cliente = aux;
+                    break;
+                }
+            }
+
+            return cliente;
         }
 }
