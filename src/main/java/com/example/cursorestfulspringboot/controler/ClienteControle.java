f4b8655ed9ec4f4169f76cce408c60969f98631b
Raissa <raissaliramiranda@hotmail.com>
2020-09-13 10:52:40 -0300

    Fix resource name cliente to clientes

--- src/main/java/com/example/cursorestfulspringboot/controler/ClienteControle.java
+++ src/main/java/com/example/cursorestfulspringboot/controler/ClienteControle.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClienteControle {
     
-    @GetMapping("/cliente")
+    @GetMapping("/clientes")
         public String getClientes(){
             return "Vai retornar todos os clientes";
         }
     
 
-    @GetMapping("/cliente/{codigo}")
+    @GetMapping("/clientes/{codigo}")
         public String getClienteByCodigo(@PathVariable int codigo){
             return "Vai retornar o cliente de codigo: " + codigo;
         }
